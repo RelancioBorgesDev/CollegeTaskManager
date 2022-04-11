e9abdec80da2187cb45e8575676126c6ddb74e3c
RelancioBorgesDev <relancio.dev@gmail.com>
2022-04-10 23:00:38 -0300

    Estrutura Básica

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './pages/App'
+import './styles/GlobalStyle.scss'
+import './styles/_variables.scss'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
